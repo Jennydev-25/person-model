4f708b85e8a014f98d7b2113a84869c103847e03
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-07-12 15:00:53 +0200

    feat: implement age calculation

--- src/main/java/dev/jenny/person/Person.java
+++ src/main/java/dev/jenny/person/Person.java
@@ -1,17 +1,21 @@
 package dev.jenny.person;
 
+import java.time.Year;
+
 public class Person {
 
     private String name;
     private String lastName;
     private String document;
     private int birthYear;
+    private int age;
 
     public Person(String name, String lastName, String document, int birthYear) {
         this.name = name;
         this.lastName = lastName;
         this.document = document;
         this.birthYear = birthYear;
+        this.age = calculateAge();
     }
 
     public String getName() {
@@ -28,5 +32,15 @@ public class Person {
 
     public int getBirthYear() {
         return this.birthYear;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public int calculateAge() {
+        int currentYear = Year.now().getValue();
+        int age = currentYear - this.birthYear;
+        return age;
     }
 }
