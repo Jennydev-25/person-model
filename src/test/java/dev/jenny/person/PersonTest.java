00761e7635738db1a483349ff9c0503053bac448
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-07-24 09:48:37 +0200

    refactor: use BeforeEach and explicit matchers in PersonTest

--- src/test/java/dev/jenny/person/PersonTest.java
+++ src/test/java/dev/jenny/person/PersonTest.java
@@ -1,56 +1,50 @@
 package dev.jenny.person;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.isA;
 
 import java.time.Year;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class PersonTest {
+public class PersonTest {
 
-    @Test
-    void testConstructorCreatesPerson() {
-        Person person = new Person("Jenny", "Sanchez", "12345678Z", 1995);
+    private Person person;
 
-        assertThat(person, is(notNullValue()));
+    @BeforeEach
+    void setUp() {
+        person = new Person("Jenny", "Sanchez", "12345678Z", 1995);
     }
 
     @Test
-    void testGetNameReturnsName() {
-        Person person = new Person("Jenny", "Sanchez", "12345678Z", 1995);
-
-        assertThat(person.getName(), is("Jenny"));
+    void testGetName() {
+        assertThat(person.getName(), isA(String.class));
+        assertThat(person.getName(), is(equalTo("Jenny")));
     }
 
     @Test
-    void testGetLastNameReturnsLastName() {
-        Person person = new Person("Jenny", "Sanchez", "12345678Z", 1995);
-
-        assertThat(person.getLastName(), is("Sanchez"));
+    void testGetLastName() {
+        assertThat(person.getLastName(), isA(String.class));
+        assertThat(person.getLastName(), is(equalTo("Sanchez")));
     }
 
     @Test
-    void testGetDocumentReturnsDocument() {
-        Person person = new Person("Jenny", "Sanchez", "12345678Z", 1995);
-
-        assertThat(person.getDocument(), is("12345678Z"));
+    void testGetDocument() {
+        assertThat(person.getDocument(), isA(String.class));
+        assertThat(person.getDocument(), is(equalTo("12345678Z")));
     }
 
     @Test
-    void testGetBirthYearReturnsBirthYear() {
-        Person person = new Person("Jenny", "Sanchez", "12345678Z", 1995);
-
-        assertThat(person.getBirthYear(), is(1995));
+    void testGetBirthYear() {
+        assertThat(person.getBirthYear(), isA(Integer.class));
+        assertThat(person.getBirthYear(), is(equalTo(1995)));
     }
 
     @Test
-    void testGetAgeReturnsAgeCalculatedFromBirthYear() {
-        int birthYear = 1995;
-        int expectedAge = Year.now().getValue() - birthYear;
-
-        Person person = new Person("Jenny", "Sanchez", "12345678Z", birthYear);
-
-        assertThat(person.getAge(), is(expectedAge));
+    void testGetAge() {
+        assertThat(person.getAge(), isA(Integer.class));
+        assertThat(person.getAge(), is(equalTo(Year.now().getValue() - 1995)));
     }
 }
